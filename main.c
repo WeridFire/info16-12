ae40d585676e391751aab081c61bb53ee9631392
Maretti Filippo <69721649+WeridFire@users.noreply.github.com>
2022-12-16 15:48:01 +0100

    hi

--- main.c
+++ main.c
@@ -1,7 +1,14 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
 int main(){
+
+	char file_name[20];
+
+	printf("\n\nInserisci nome del file: ");
+
+	scanf("%s", file_name);
 
 
 	return 0;
